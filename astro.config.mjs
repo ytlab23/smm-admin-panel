e85ba1f7de8ebf67ae4e993db5c7a4f87ebaf085
Bishal Biswas <bbiswas47@outlook.com>
2024-10-19 17:46:55 +0530

    Issues Fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   integrations: [tailwind()],
   // site: 'http://localhost:4321',
   // site: 'https://smm-reviews-panel-v2.vercel.app/',
-  site: 'https://smm-admin.vercel.app/',
+  site: 'https://smm-admin-panel.vercel.app/',
   // site: 'https://smm-admin-phi.vercel.app/',
   output: "server",
   adapter: vercel()
