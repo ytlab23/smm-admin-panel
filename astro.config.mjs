898802e1f2ae3d97b2105a8de3d6a42c97956d6d
Bishal Biswas <bbiswas47@outlook.com>
2025-01-23 14:38:14 +0530

    Update and improve admin login logic

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ let site = "https://smm-admin.vercel.app/"
 export default defineConfig({
   integrations: [tailwind()],
   // site: 'http://localhost:4321',
-  // site: 'http://localhost:4321',
   // site: 'https://smm-reviews-panel-v2.vercel.app/',
   site: 'https://smm-admin-panel.vercel.app/',
   // site: 'https://smm-admin-phi.vercel.app/',
--- src/pages/api/adminLogin/index.json.ts
+++ src/pages/api/adminLogin/index.json.ts
@@ -5,62 +5,80 @@ const xata = new XataClient({ apiKey: import.meta.env.XATA_API_KEY, branch: impo
 
 export const POST: APIRoute = async ({ request }) => {
 
-    const userData = await request.formData();
-    // console.log(userData);
-    // console.log(userData.get("g-recaptcha-response")?.toString());
+    try {
+        const userData = await request.formData();
+        // console.log(userData);
+        // console.log(userData.get("g-recaptcha-response")?.toString());
 
-    const recaptchaURL = 'https://www.google.com/recaptcha/api/siteverify';
-    const requestHeaders = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    };
-    const requestBody = new URLSearchParams({
-        secret: import.meta.env.CapchaSecretKey,   // This can be an environment variable
-        response: userData.get("g-recaptcha-response")?.toString() || ""          // The token passed in from the client
-    });
+        const recaptchaURL = 'https://www.google.com/recaptcha/api/siteverify';
+        const requestHeaders = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+        const requestBody = new URLSearchParams({
+            secret: import.meta.env.CapchaSecretKey,   // This can be an environment variable
+            response: userData.get("g-recaptcha-response")?.toString() || ""          // The token passed in from the client
+        });
 
-    const response = await fetch(recaptchaURL, {
-        method: "POST",
-        headers: requestHeaders,
-        body: requestBody.toString()
-    });
+        const response = await fetch(recaptchaURL, {
+            method: "POST",
+            headers: requestHeaders,
+            body: requestBody.toString()
+        });
 
-    const responseData = await response.json();
-    // console.log("Captcha Response- ", responseData);
+        const responseData = await response.json();
+        // console.log("Captcha Response- ", responseData);
 
-    // return new Response(JSON.stringify(responseData), { status: 200 });
+        // return new Response(JSON.stringify(responseData), { status: 200 });
 
-    const userID = await xata.db.users.search(userData.get("username")?.toString() || "", {
-        fuzziness: 0,
-        target: ['username']
-    })
+        const records = await xata.db.users.select(["username", "password"]).getAll();
 
-    if (!responseData.success) {
-        return new Response(JSON.stringify({
-            message: "reCAPTCHA verification failed",
-            captchaRes : responseData,
-            'error-codes': responseData['error-codes'],
-            status: 400,
-        }), {
-            status: 400,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-    }
-    else {
-        if (userID.totalCount == 1 && userID.records[0].password == userData.get("password")?.toString()) {
-            return new Response(JSON.stringify({
-                message: "user found",
-                userID: userID.records[0].id,
-                status: 200,
-                captcha: responseData
-            }))
+        let foundUser;
+        for (let i = 0; i < records.length; i++) {
+            foundUser = records[i];
         }
-        else
+
+        /* const userID = await xata.db.users.search(userData.get("username")?.toString() || "", {
+            fuzziness: 0,
+            target: ['username']
+        }) */
+
+        if (!responseData.success) {
             return new Response(JSON.stringify({
-                message: "user not found",
-                status: 304,
-                captcha: responseData
-            }))
+                message: "reCAPTCHA verification failed",
+                captchaRes : responseData,
+                'error-codes': responseData['error-codes'],
+                status: 400,
+            }), {
+                status: 400,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+        }
+        else {
+            // if (userID.totalCount == 1 && userID.records[0].password == userData.get("password")?.toString()) {
+            if (foundUser != null && foundUser.password == userData.get("password")?.toString()) {
+                return new Response(JSON.stringify({
+                    message: "user found",
+                    // userID: userID.records[0].id,
+                    userID: foundUser.id,
+                    status: 200,
+                    captcha: responseData
+                }))
+            }
+            else
+                return new Response(JSON.stringify({
+                    message: "user not found",
+                    status: 304,
+                    captcha: responseData
+                }))
+        }
+    } catch (error) {
+        return new Response(JSON.stringify({
+            message: "Login Failed",
+            error: JSON.stringify(error),
+            status: 500,
+        }))
     }
+    
 }
